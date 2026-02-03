57eb37bc225003f35c56637bba92aac692909b1f
shahensaifullah <shahriar350@gmail.com>
2026-02-03 19:52:18 +0100

    added axios as a instance

---
+++ lib/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: true, // only if you need cookies
+});
+
+// Optional interceptors
+api.interceptors.request.use(
+    (config) => {
+        // Example: attach token
+        // const token = localStorage.getItem("token");
+        // if (token) config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => Promise.reject(error)
+);
+
+export default api;
